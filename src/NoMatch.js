ba4a99190eac90575224ab37d6bd693a52da2c40
wlunareve <wlunareve@gmail.com>
2018-12-08 23:14:06 +0800

    Install react-bootstrap and trying plus some css style

---
+++ src/NoMatch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Nomatch extends Component{
+    render(){
+        return(
+            <h1>Page Not Found</h1>
+        );
+    }
+}
+
+export default Nomatch;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 //import history from './history';
 import './index.css';
 import App from './App';
@@ -10,21 +10,32 @@ import ReactBasic from './ReactBasic';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import Nomatch from './NoMatch';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-        <div>
-            <ul>
-                <li><Link to ="/App">App</Link></li>
-                <li><Link to ="/ReactBasic">ReactBasic</Link></li>
-            </ul>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-2 align--start">
+                    <Link to ="/Home" class="btn btn-block btn-info">Home</Link>
+                </div>
+                <div class="col-2 offset-md-6">
+                    <Link to ="/App" class="btn btn-block btn-info">App</Link>
+                </div>
+                <div class="col-2">
+                    <Link to ="/ReactBasic" class="btn btn-block btn-info">ReactBasic</Link>
+                </div>
+            </div>
+            <Switch>
             <Route path="/App" component={App}>    
             </Route>
             <Route path="/ReactBasic" component={ReactBasic}>    
             </Route>
+            <Route component={Nomatch}></Route>
+            </Switch>
         </div>
         </BrowserRouter>
     </Provider>,
