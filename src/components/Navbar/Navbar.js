10a6e8cef9eeb5396e69b38a2ff81c96aaeb9e63
wlunareve <wlunareve@gmail.com>
2018-12-25 16:00:48 +0800

    Add twitch in bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import NavbarButton from './NavbarButton';
+import NavbarButton from './NavbarButton'
 
 class Navbar extends Component{
     render(){
         return(
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -14,6 +14,7 @@ class Navbar extends Component{
                     <NavbarButton route="/Home" name="Home"></NavbarButton>
                     <NavbarButton route="/App" name="App"></NavbarButton>
                     <NavbarButton route="/Reactbasic" name="Reactbasic"></NavbarButton>
+                    <NavbarButton route="/Twitch" name="Twitch"></NavbarButton>
                 </div>
             </div>
         </nav>
