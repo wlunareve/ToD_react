fa661a7c023acaa08d777eb88aa2c0dbc1520890
wlunareve <wlunareve@gmail.com>
2019-01-03 16:55:29 +0800

    Fix class to className

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,13 +4,13 @@ import NavbarButton from './NavbarButton'
 class Navbar extends Component{
     render(){
         return(
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a href="/Home" class="navbar-brand">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a href="/Home" className="navbar-brand">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
                     <NavbarButton route="/Home" name="Home"></NavbarButton>
                     <NavbarButton route="/App" name="App"></NavbarButton>
                     <NavbarButton route="/Reactbasic" name="Reactbasic"></NavbarButton>
--- src/components/Navbar/NavbarButton.js
+++ src/components/Navbar/NavbarButton.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class NavbarButton extends Component{
     render(){
         return(
-            <div class="nav-item">
+            <div className="nav-item">
                 <Link to ={this.props.route} class="btn btn-block btn-outline-success my-2 mr-sm-2">{this.props.name}</Link>
             </div>
         );
