1c39bea9ad4c0cf938c70a986b45d1aea50ba1bb
wlunareve <wlunareve@gmail.com>
2019-01-04 17:03:38 +0800

    Add placeholder

--- src/components/twitchPractice/twich_box.js
+++ src/components/twitchPractice/twich_box.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
 class TwitchBox extends Component{
+
+    handleImageLoaded(){
+        for(let img of document.querySelectorAll('#img')){
+            img.style.opacity=1
+        }
+    }
+
     render(){
         return(
             <div className="twitch_col">
                 <div className="preview">
+                    <div className="placeholder">
+                    </div>
                     <a href={this.props.link}>
-                    <img src={this.props.preview} alt="我是圖片"></img>
+                    <img id='img' src={this.props.preview} alt="我是圖片" onLoad={this.handleImageLoaded}></img>
                     </a>
                 </div>
                 <div className="twitch_bottom">
